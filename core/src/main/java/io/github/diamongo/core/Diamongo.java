a52d987bda78eb5871559d2d10209f2508122b5f
Jovica Zoric <zoricjovica@gmail.com>
2017-03-13 17:01:01 +0100

    Create stub Diamongo class
    Diamongo contains:
    * migrate
    * status
    * validate
    * clear
    
    methods.
    
    Fixes: #2
    Change-Id: Ia82cfbbc12a977fdae61c3197de5a134f4f20742

---
+++ core/src/main/java/io/github/diamongo/core/Diamongo.java
@@ -0,0 +1,50 @@
+/*
+ *
+ * Copyright © 2017 The Diamongo authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.diamongo.core;
+
+/**
+ * Entrypoint for Diamongo.
+ */
+public class Diamongo {
+    /**
+     * Starts the database migration.
+     */
+    public void migrate() {
+        // NO-OP
+    }
+
+    /**
+     * Retrieves the information about all the migrations.
+     */
+    public void status() {
+        // NO-OP
+    }
+
+    /**
+     * Validates migrations.
+     */
+    public void validate() {
+        // NO-OP
+    }
+
+    /**
+     * Clears all migrations.
+     */
+    public void clear() {
+        // NO-OP
+    }
+}
