919c4b36af5b512d75d059f16e20f78bbfaa4cd9
Jovica Zoric <zoricjovica@gmail.com>
2017-03-13 17:01:01 +0100

    Create stub Diamongo class
    Diamongo contains:
    * migrate
    * status
    * validate
    * clear
    
    methods.
    
    Fixes: #2
    Change-Id: Ia82cfbbc12a977fdae61c3197de5a134f4f20742

---
+++ core/src/main/java/io/github/diamongo/core/Diamongo.java
@@ -0,0 +1,34 @@
+package io.github.diamongo.core;
+
+/**
+ * Entrypoint for Diamongo.
+ */
+public class Diamongo {
+    /**
+     * Start the database migration.
+     */
+    public void migrate() {
+        // NO-OP
+    }
+
+    /**
+     * Get status about all migrations
+     */
+    public void status() {
+        // NO-OP
+    }
+
+    /**
+     * Validate migrations
+     */
+    public void validate() {
+        // NO-OP
+    }
+
+    /**
+     * Clear all migrations
+     */
+    public void clear() {
+        // NO-OP
+    }
+}
---
+++ core/src/test/java/io/github/diamongo/core/DiamongoTest.java
@@ -0,0 +1,5 @@
+package io.github.diamongo.core;
+
+public class DiamongoTest {
+
+}
