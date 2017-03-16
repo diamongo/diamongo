0e9da9a1662f38213f59db2d882f346761ca2e38
Reinhard Nägele <reinhard.naegele@codecentric.de>
2017-03-16 21:36:29 +0100

    Add annotation processor for hashing Java change sets
    Change-Id: I59a663c47ca5cb8926eabbf61bedb80bde5a1169
    Closes: #9

---
+++ integration-tests/src/test/java/io/github/diamongo/integationtests/V1_0__TestChangeSetTest.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * Copyright © 2017 The Diamongo authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.github.diamongo.integationtests;
+
+import io.github.diamongo.integrationtests.V1_0__TestChangeSet_sha256;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class V1_0__TestChangeSetTest {
+
+    @Test
+    public void testSha256Correct() throws Exception {
+        String expected = "8b689d189307b9fe920251719dc6b95823866cb8ecf65e5979b244519f38ef0d";
+        assertThat(V1_0__TestChangeSet_sha256.SHA256).isEqualTo(expected);
+    }
+}
