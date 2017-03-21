863734ed011f244dec416d9cc3def99651a577ac
Reinhard Nägele <reinhard.naegele@codecentric.de>
2017-03-21 21:43:37 +0100

    Fix license header
    Change-Id: I6078fa15b353793970527a871d37b319e3c9f8db
    Closes: #11

--- integration-tests/src/test/java/io/github/diamongo/integationtests/CodeGenerationTest.java
+++ integration-tests/src/test/java/io/github/diamongo/integationtests/CodeGenerationTest.java
@@ -1,15 +1,17 @@
 /*
- *
  * Copyright © 2017 The Diamongo authors. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.github.diamongo.integationtests;
 
@@ -29,8 +31,8 @@ public class CodeGenerationTest {
     @Test
     public void testSha256Correct() throws Exception {
         Map<Class<? extends Migration>, String> checksums = ImmutableMap.of(
-            V1_0__TestChangeSet.class, "a096a76574c91b10b9fb5413774783ac65758e0cfc559be966a9cca6548acea0",
-            V2_0__TestChangeSet.class, "78a5af98299d69581a5defb8214fefc53d75f74bcbc36f0670e9f059c4c13bdb"
+            V1_0__TestChangeSet.class, "b1f2ac01bf7f435711dff9ec5c008fe7d1dcae99028b60f243e51e4abfb546e5",
+            V2_0__TestChangeSet.class, "2cf773673e833aa651a40ce3b20f7dd0582edd4ef9b722bec44986e6847913d5"
         );
 
         MigrationWrappers wrappers = new MigrationWrappers();
